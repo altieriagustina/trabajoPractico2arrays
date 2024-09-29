ae7cea9eb77dadf6fd2a58e589773c207b7305f7
altieriagustina <altieriagustina@gmail.com>
2024-09-29 12:03:06 -0300

    Trabajo obligatorio 2

--- tiposDeNros.ts
+++ tiposDeNros.ts
@@ -2,17 +2,21 @@ import * as rs from "readline-sync"
 
 let dim: number = rs.questionInt(`Ingrese la dimension del arreglo: `);
 let arregloInvertido: number[] = new Array(dim);
-let positivos : number = 0;
-let negativos : number = 0;
-let cero : number = 0;
+let positivos: number = 0;
+let negativos: number = 0;
+let cero: number = 0;
 
 for (let i = 0; i < dim; i++) {
-arregloInvertido[i] = rs.questionInt(`Ingrese el numero de la posicion ${i+1}: `)
-if (arregloInvertido[i] > 0) {
-    positivos + 1;
-} else if (arregloInvertido[i] < 0) {
-    negativos + 1
-} else {
-    cero + 1
+    arregloInvertido[i] = rs.questionInt(`Ingrese el numero de la posicion ${i + 1}: `)
+    if (arregloInvertido[i] > 0) {
+        positivos = positivos + 1;
+    } else if (arregloInvertido[i] < 0) {
+        negativos = negativos + 1
+    } else {
+        cero = cero + 1
+    }
 }
-}
+
+console.log(`Cantidad de numeros positivos: ${positivos}`)
+console.log(`Cantidad de numeros negativos: ${negativos}`)
+console.log(`Cantidad de ceros: ${cero}`)
